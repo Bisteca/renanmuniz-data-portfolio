e1d292e8db7340800f16dc616afc71f0faebf10f
Bisteca <luregat@gmail.com>
2025-12-28 16:25:25 +0100

    .

---
+++ Atendimentos/Atentimentos/variables.py
@@ -0,0 +1,17 @@
+import os
+import psycopg2
+
+def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+    if database_url:
+        return psycopg2.connect(database_url)
+
+    return psycopg2.connect(
+        dbname="controle_atendimentos",
+        user="postgres",
+        password="Renan1013",
+        host="localhost",
+        port="5432"
+    )
+
+conexion = get_connection()
